aada4ae5e4e3d305dd6595b2df96b17380b4dfa5
francisco <lucas.araripina@hotmail.com>
2024-05-24 22:01:39 -0300

    feat: criando controller para request de artigos principais

---
+++ src/controllers/artigosPrincipaisController.js
@@ -0,0 +1,17 @@
+exports.getArtigosPrincipais = async function (req, res) {
+
+  try {
+
+    const data = require('../data/artigosPrincipaisData');
+
+    if (data.error) {
+        return res.status(500).json({ message: "Erro ao carregar artigos principais.", details: data.error });
+    }
+
+    return res.json(data);
+
+  } catch (error) {
+    return res.status(500).json({ message: "Erro ao processar a solicitação.", details: error.message });
+  }  
+};
+    
