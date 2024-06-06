c323b451a7525793532d9d408310894a7ccb9160
francisco <lucas.araripina@hotmail.com>
2024-06-06 16:54:52 -0300

    feat: ajustando cors

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3100;
 
+app.use(cors());
 require('./routes/imagesRoutes')(app);
 require('./routes/artigosprincipaisRoutes')(app);
 require('./routes/artigosSecundariosRoute')(app);
 require('./routes/artigoDetalhadoRoutes')(app);
 
+
 app.use(express.static('../public'));
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
 });
